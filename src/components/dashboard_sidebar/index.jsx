3aa9de273acfd919e3afadf29154f1ef012844ca
jordanbmnt <jordanbmnt@gmail.com>
2025-07-10 17:29:34 +0200

    fixed user dp

--- src/components/dashboard_sidebar/index.jsx
+++ src/components/dashboard_sidebar/index.jsx
@@ -58,7 +58,7 @@ const DashboardSidebar = () => {
                     <span className='sr-only'>Open user menu</span>
                     <img
                       className='w-8 h-8 rounded-full'
-                      src='/'
+                      src='https://flowbite.com/docs/images/people/profile-picture-5.jpg'
                       alt='user'
                     />
                   </button>
