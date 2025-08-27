7468071f351da4515e58755c3779af95e97c0fb5
jordanbmnt <jordanbmnt@gmail.com>
2025-08-27 11:42:56 +0200

    updated text styles for DashboardHeader

--- src/pages/dashboard/dashboard_header/index.jsx
+++ src/pages/dashboard/dashboard_header/index.jsx
@@ -8,7 +8,7 @@ export const DashboardHeader = () => {
     <div className="m-auto mt-0 w-[70vw] p-15">
       <div className="flex flex-row justify-around align-center h-[40vh] w-[60vw] m-auto">
         <img src={darkModeLogo} alt="" />
-        <h1 className={`text-5xl m-auto font-bold ${STYLES.dark.text.primary}`}>Find the Perfect Candidate</h1>
+        <h1 className={`text-5xl m-auto font-bold ${STYLES.dark.text.primary} ${STYLES.dark.text.primary}`}>Find the Perfect Candidate</h1>
       </div>
       <SearchBar />
     </div>
