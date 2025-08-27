df9ddc91bfcd102cf32c141a5cf355dd7bd2dde5
jordanbmnt <jordanbmnt@gmail.com>
2025-08-27 10:52:00 +0200

    updated text styles for ProgressBar

--- src/pages/cv_link/progress_bar/index.jsx
+++ src/pages/cv_link/progress_bar/index.jsx
@@ -1,3 +1,5 @@
+import { STYLES } from "../../../constants/styles";
+
 export const ProgressBar = ({ steps, currentStep, getCurrentStep }) => {
   return (
     <div className="max-w-2xl mx-auto mb-8">
@@ -10,9 +12,9 @@ export const ProgressBar = ({ steps, currentStep, getCurrentStep }) => {
             <div key={step.id} className={`flex items-center ${index === steps.length - 1 ? "w-fit" : "w-full"}`}>
               <div
                 className={`min-w-10 min-h-10 rounded-full flex items-center justify-center text-sm font-medium transition-colors shadow-md ${isCompleted
-                  ? "bg-green-600 text-white"
+                  ? `bg-green-600 ${STYLES.dark.text.primary}`
                   : isCurrent
-                    ? "bg-blue-600 text-white"
+                    ? `bg-blue-600 ${STYLES.dark.text.primary}`
                     : "bg-gray-200 text-gray-500"
                   }`}
               >
@@ -20,7 +22,7 @@ export const ProgressBar = ({ steps, currentStep, getCurrentStep }) => {
               </div>
               {index < steps.length - 1 && (
                 <div
-                  className={`h-1 w-full mx-4 transition-colors shadow-sm ${isCompleted ? "bg-green-600" : "bg-gray-200"
+                  className={`h-1 w-full mx-4 transition-colors shadow-sm ${isCompleted ? "bg-green-600" : STYLES.dark.text.secondary
                     }`}
                 />
               )}
@@ -29,10 +31,10 @@ export const ProgressBar = ({ steps, currentStep, getCurrentStep }) => {
         })}
       </div>
       <div className="text-center">
-        <h3 className="text-lg font-semibold text-gray-400">
+        <h3 className={`text-lg font-semibold ${STYLES.dark.text.tertiary}`}>
           Step {currentStep}: {getCurrentStep().title}
         </h3>
-        <p className="text-sm text-gray-500 mt-1">
+        <p className={`text-sm ${STYLES.dark.text.paragraph} mt-1`}>
           {getCurrentStep().description}
         </p>
       </div>
