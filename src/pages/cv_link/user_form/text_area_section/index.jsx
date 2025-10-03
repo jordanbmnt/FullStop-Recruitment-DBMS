9105712a0f0909f0d5cb8166d10147b8190e46b4
jordanbmnt <jordanbmnt@gmail.com>
2025-10-03 16:05:00 +0200

    formatted

--- src/pages/cv_link/user_form/text_area_section/index.jsx
+++ src/pages/cv_link/user_form/text_area_section/index.jsx
@@ -1,13 +1,22 @@
 import { STYLES } from "../../../../constants/styles";
 
-export const TextAreaSection = ({ formData, inputHandler, field, heading, placeholder, isRequired }) => {
+export const TextAreaSection = ({
+  formData,
+  inputHandler,
+  field,
+  heading,
+  placeholder,
+  isRequired,
+}) => {
   return (
     <div>
-      <label className={`block text-sm font-medium ${STYLES.dark.text.paragraph} mb-2`}>
-        {heading} {isRequired && <span class='text-red-500'>*</span >}
+      <label
+        className={`block text-sm font-medium ${STYLES.dark.text.paragraph} mb-2`}
+      >
+        {heading} {isRequired && <span className='text-red-500'>*</span>}
       </label>
       <textarea
-        value={formData || ''}
+        value={formData || ""}
         onChange={(e) => inputHandler(field, e.target.value)}
         placeholder={placeholder}
         className={`w-full p-4 border ${STYLES.dark.border.medium} rounded-lg focus:ring-2 focus:ring-[${STYLES.dark.accent.color}] focus:border-transparent resize-none ${STYLES.dark.background.tertiary} ${STYLES.dark.text.paragraph} placeholder:text-gray-600`}
@@ -15,4 +24,4 @@ export const TextAreaSection = ({ formData, inputHandler, field, heading, placeh
       />
     </div>
   );
-}
+};
